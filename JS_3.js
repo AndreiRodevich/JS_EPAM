b0b2e325a052d8c98fd8610693c13a066f3fa25c
Andrei <andreirodevich56.com>
2021-12-05 13:52:40 +0300

    Conditional Statements and Data Types

---
+++ JS_3.js
@@ -0,0 +1,74 @@
+//Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+//var a = "it is ";
+//var value = true;
+//c = String(value);
+//var b = a + c;
+//console.log(b);
+//alert(b);
+
+//var a = 1;
+//var c = "abc";
+//var x = a + c;
+//console.log(x);
+//alert(x);
+
+//var a = 1;
+//var value = true;
+//c = Number(value);
+//var b = a + c;
+//console.log(b);
+//alert(b);
+
+//Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+
+//var a = "1";
+//var b = true;
+//an = Number(a);
+//bn = Number(b);
+//var c = an * bn;
+//console.log(c);
+//alert(c);
+
+//var a = "1";
+//var b = 2;
+//an = Number(a);
+//var c = an * b;
+//console.log(c);
+//alert(c);
+
+//var a = 1;
+//var b = true;
+//bn = Number(b);
+//var c = a * bn;
+//console.log(c);
+//alert(c);
+
+//Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+
+//var a = "1";
+//var b = true;
+//an = Number(a);
+//bn = Number(b);
+//var c = an / bn;
+//console.log(c);
+//alert(c);
+
+//var a = "1";
+//var b = 2;
+//an = Number(a);
+//var c = an / b;
+//console.log(c);
+//alert(c);
+
+//var a = 1;
+//var b = true;
+//bn = Number(b);
+//var c = a / bn;
+//console.log(c);
+//alert(c);
+
+
+
+
+
+
